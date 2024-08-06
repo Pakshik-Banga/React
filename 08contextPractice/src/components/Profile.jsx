a1624bb837633ea10aecb8bb18ae27a7a91de309
Pakshik-Banga <114055506+Pakshik-Banga@users.noreply.github.com>
2024-08-06 19:21:06 +0530

    Context API

---
+++ 08contextPractice/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React , {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+
+const {user} = useContext(UserContext)
+
+if(!user){
+  return <h1>Not loged in </h1>
+}
+
+  return (
+    <div>Profile : {user.username}</div>
+    
+  )
+}
+
+export default Profile
